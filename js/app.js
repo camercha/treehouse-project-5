c9b7d3bb38cf24a08b0cc41fb8bc5a844316570a
Cameron Chandler <cameronchandler110@gmail.com>
2020-11-27 01:16:33 -0500

    I got the search function to work

--- js/app.js
+++ js/app.js
@@ -7,34 +7,29 @@ let input;
 let a;
 let i;
 let textValue;
+let title;
 
 
 document.getElementById('searchbar').addEventListener('keyup', myFunction);
 function myFunction() {
     input = document.getElementById('searchbar');
-    input.value = input.value.toLowerCase();
-    // href = document.getElementsByClassName("gallery")
-    // data = href.getElementsByTagName('a');
+    filter = input.value.toLowerCase();
+    a = document.getElementsByTagName('a');
     console.log(input.value);
 
 
-    // for (i = 0; i < data.length; i++) {
-    //     a = data[i].getAttribute("data-caption")[0];
-    //     textValue = a.textContent || a.innerText;
-    //     if (textValue.toLowerCase().indexOf(input) > -1) {
-    //         data[i].style.display = "";
-    //     } else {
-    //         data[i].style.display = "none"
-    //     }
-    // }
+    for (i = 0; i < a.length; i++) {
+        title = a[i].getAttribute('data-caption');
+        if (title.toLowerCase().indexOf(filter) > -1) {
+            a[i].style.display = '';
+        } else {
+            a[i].style.display = 'none';
+        }
+    }
+
 
 
 
 }
-function myFunction2() {
-    let cap = document.getElementsByTagName('a')[0].getAttribute('data-caption');
-    cap.value
-}
-
 
 
