c43636093177268ca494afba5fc32694811a61f1
Cameron Chandler <cameronchandler110@gmail.com>
2020-11-26 22:44:28 -0500

    cleaned up the style now trying to build the search bar

--- js/app.js
+++ js/app.js
@@ -2,3 +2,24 @@ baguetteBox.run('.gallery');
 
 
 
+
+
+
+
+
+// function myFunction() {
+//     let = input;
+//     input = document.getElementById('searchbar');
+//     return console.log(myFunction());
+//     upper = input.value.toUppercase();
+
+
+
+// }
+
+// for (i = 0; i < img.length; i++) {
+//     a = img[i].getElementsByTagName('a')[0];
+// }
+
+
+
